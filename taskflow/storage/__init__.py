407195495a1e09a0c35c57715cd4a1af2ddbf241
Sasha Malahov <sasha@compsci.boutique>
2026-08-07 23:22:34 -0400

    feat: add storage package init

--- taskflow/storage/__init__.py
+++ taskflow/storage/__init__.py
@@ -1,5 +1,15 @@
-"""Storage module for TaskFlow."""
+"""Storage layer for TaskFlow."""
 
-from taskflow.storage.sqlite_store import SQLiteStore
+from taskflow.storage.file_store import FileStore
+from taskflow.storage.ticket_store import TicketStore
+from taskflow.storage.user_store import UserStore
+from taskflow.storage.label_store import LabelStore
+from taskflow.storage.notification_store import NotificationStore
 
-__all__ = ["SQLiteStore"]
+__all__ = [
+    "FileStore",
+    "TicketStore",
+    "UserStore",
+    "LabelStore",
+    "NotificationStore",
+]
